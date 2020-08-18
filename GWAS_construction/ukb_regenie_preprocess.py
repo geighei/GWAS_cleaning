189008282a25a621aa9df3ef4b6640da5c6f0f3f
jremedy <jdvollen@gmail.com>
2020-08-18 15:59:07 +0000

    regenie steps 1 & 2 test

--- GWAS_construction/ukb_regenie_preprocess.py
+++ GWAS_construction/ukb_regenie_preprocess.py
@@ -9,5 +9,5 @@ first_wave["IID"] = first_wave["v_eid"]
 phenos = first_wave[["FID", "IID", "drinks_per_week"]]
 covars = first_wave[["FID", "IID", "male", "year_of_birth", "PC_1", "PC_2", "PC_3", "PC_4", "PC_5", "PC_6", "PC_7", "PC_8", "PC_9", "PC_10"]]
 
-phenos.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/dpw_phenos.txt", sep="\t", index=False)
-covars.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/ukb_covars.txt", sep="\t", index=False)
+phenos.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/dpw_phenos.txt", sep="\t", index=False, na_rep="NA")
+covars.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/ukb_covars.txt", sep="\t", index=False, na_rep="NA")
