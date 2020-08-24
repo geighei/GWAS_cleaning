f63d326f6c4f5f309ea18a9a5f99c48483a32e4c
jremedy <jdvollen@gmail.com>
2020-08-24 20:58:50 +0000

    pre-processing selects first available dpw

--- GWAS_construction/ukb_regenie_preprocess.py
+++ GWAS_construction/ukb_regenie_preprocess.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 panel = pd.read_csv("/home/ubuntu/biroli/geighei/code/alcoholGxE/inputs/alcohol_analysis_panel.csv")
+# include first non-missing wave for each sample, throwing out all obs for which we don't have dpw
+non_missing = panel.dropna(subset=["drinks_per_week"]).groupby(["v_eid"], as_index=False).first()
 
-first_wave = panel[panel.i == 1]
-first_wave["FID"] = first_wave["v_eid"]
-first_wave["IID"] = first_wave["v_eid"]
+non_missing["FID"] = non_missing["v_eid"]
+non_missing["IID"] = non_missing["v_eid"]
 
-phenos = first_wave[["FID", "IID", "drinks_per_week"]]
-covars = first_wave[["FID", "IID", "male", "year_of_birth", "PC_1", "PC_2", "PC_3", "PC_4", "PC_5", "PC_6", "PC_7", "PC_8", "PC_9", "PC_10"]]
+phenos = non_missing[["FID", "IID", "drinks_per_week"]]
+covars = non_missing[["FID", "IID", "male", "year_of_birth", "PC_1", "PC_2", "PC_3", "PC_4", "PC_5", "PC_6", "PC_7", "PC_8", "PC_9", "PC_10"]]
 
 phenos.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/dpw_phenos.txt", sep="\t", index=False, na_rep="NA")
 covars.to_csv("/home/ubuntu/biroli/geighei/data/GWAS_sumstats/construction/ukb_covars.txt", sep="\t", index=False, na_rep="NA")
