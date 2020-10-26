24d727d3c88acf2e661cff6715357467d5a27bc5
jremedy <jdvollen@gmail.com>
2020-10-26 14:46:24 +0000

    cleaned up errors in regenie batch code; working version running

--- construction/split_sample_batch.py
+++ construction/split_sample_batch.py
@@ -18,42 +18,40 @@ geno_fp = "/home/ubuntu/UKB/genomeclean/ukb_non_imputed_best_guess_QC"
 pheno = "dpw"
 # sub-folder of this phenotypes folder where we will store results temporarily and write out folds
 sub_folder = "split_sample"
-out_fp = "/home/ubuntu/biroli/geighei/data/GWAS_sumstats/clean/UKB/dpw/ukb_dpw_splitA.txt"
+out_fp = "/home/ubuntu/biroli/geighei/data/GWAS_sumstats/clean/UKB"
 
 ### INTERMEDIATE SETUP --------------------- ###
 # input file paths
-# covariates file gives covars but also the selection of individuals to be used in full GWAS
-covars_fp = construction_fp + "ukb_covars.txt"
-phenos_fp = construction_fp + pheno + "/" + pheno + "_pheno.txt"
-regenie_out_fp = construction_fp + pheno + "/" + sub_folder
+# phenos file gives phenos but also the selection of individuals to be used in full GWAS
+phenos_fp = os.path.join(construction_fp, pheno, pheno+"_pheno.txt")
+regenie_out_fp = os.path.join(construction_fp, pheno, sub_folder)
 # make directory for regenie input/output if it doesn't already exist
 with suppress(FileExistsError):
 	os.makedirs(regenie_out_fp)
 
-# read fam file to get list of FID, IID pairs
-covars = pd.read_csv(covars, 
-	usecols=["FID", "IID"], delim_whitespace=True)
+# read pheno file to get list of FID, IID pairs that we want to use in analysis
+samples = pd.read_csv(phenos_fp, usecols=["FID", "IID"], delim_whitespace=True)
 # randomly re-label each sample with unique number and use these to form n uniform-sized folds
-n_samples = len(covars.index)
-covars["random"] = np.random.choice(n_samples, n_samples, replace=False)
-covars["fold"] = covars.random % n_folds
+n_samples = len(samples.index)
+samples["random"] = np.random.choice(n_samples, n_samples, replace=False)
+samples["fold"] = samples.random % n_folds
 
 ### CALL REGENIE --------------------------- ###
 for fold in np.arange(1,2): # should be np.arange(1,n_folds+1)
 	# select fold subset and write to drive
-	fold_ids = covars[covar.fold == fold][["FID", "IID"]]
-	fold_ids_fp = regenie_out_fp + "/fold" + str(fold) + ".txt"
+	fold_ids = samples[samples.fold == fold][["FID", "IID"]]
+	fold_ids_fp = os.path.join(regenie_out_fp, "fold"+str(fold)+".txt")
 	fold_ids.to_csv(fold_ids_fp, sep="\t", index=False, header=False)
 	# call script which executes regenie and compiles output according to our specification
 	cmd = ["./single_pheno_regenie.sh", 
 		"--dir", construction_fp,
-		"--bed", fold_ids_fp,
+		"--bed", geno_fp,
 		"--bgen1", "/home/ubuntu/UKB/imputed/ukb_imp_chr",
 		"--bgen2", "_v3.bgen",
 		"--sample1", "/home/ubuntu/UKB/genomeraw/ukb41382_imp_chr",
 		"--sample2", "_v3_s487330.sample",
 		"--pheno", pheno,
 		"--keep", fold_ids_fp,
-		"--tmpout", regenie_out_fp,
-		"--out", out_fp + pheno + "/ukb_" + pheno + "_split" + str(fold) + ".txt"]
+		"--tmpout", os.path.join(regenie_out_fp, "ukb_"+pheno),
+		"--out", os.path.join(out_fp, pheno, "ukb_"+pheno+"_split"+str(fold)+".txt")]
 	run = subprocess.run(cmd)
